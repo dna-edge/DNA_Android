0f74f8022bd301d6b1769e00a64bb9e36be2ca53
idjoopal <idjoopal@naver.com>
2018-10-03 15:01:11 +0900

    chat화면 RecyclerView 연결

--- app/src/main/java/com/konkuk/dna/dearneighboranyone/ActivityChat.java
+++ app/src/main/java/com/konkuk/dna/dearneighboranyone/ActivityChat.java
@@ -2,12 +2,28 @@ package com.konkuk.dna.dearneighboranyone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class ActivityChat extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    protected RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        recyclerView = (RecyclerView)findViewById(R.id.rv_chat);
+        recyclerView.setHasFixedSize(false);
+
+        //recyclerView를 linearlayout방식으로 사용하기
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new ChatRecyclerViewAdapter();
+        recyclerView.setAdapter(adapter);
     }
 }
