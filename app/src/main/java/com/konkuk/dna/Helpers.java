7108ae2870df783955e110f5a5379667e0c8fa47
SoyoungPark <3457soso@naver.com>
2018-10-04 23:33:42 +0900

    add mainActivity

--- app/src/main/java/com/konkuk/dna/Helpers.java
+++ app/src/main/java/com/konkuk/dna/Helpers.java
@@ -54,4 +54,11 @@ public class Helpers {
 
         return v;
     }
+
+    public static int dpToPx(Context context, int dp) {
+        float density = context.getResources()
+                .getDisplayMetrics()
+                .density;
+        return Math.round((float) dp * density);
+    }
 }
--- app/src/main/java/com/konkuk/dna/MainActivity.java
+++ app/src/main/java/com/konkuk/dna/MainActivity.java
@@ -1,14 +1,23 @@
 package com.konkuk.dna;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.konkuk.dna.chat.ChatActivity;
 import com.konkuk.dna.chat.ChatUser;
 import com.konkuk.dna.chat.ChatUserAdapter;
 
@@ -16,7 +25,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends BaseActivity {
     private DrawerLayout menuDrawer;
+    private View mapFragment;
 
+    private ValueAnimator slideAnimator;
+    private AnimatorSet set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +41,48 @@ public class MainActivity extends BaseActivity {
     public void init() {
         menuDrawer = findViewById(R.id.drawer_layout);
         Helpers.initDrawer(this, menuDrawer, 0);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        final int height = size.y - Helpers.dpToPx(this, 50);
+
+        mapFragment = (View) findViewById(R.id.mapFragment);
+
+        slideAnimator = ValueAnimator
+            .ofInt(height, Helpers.dpToPx(this, 150)).setDuration(300);
+
+        slideAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // get the value the interpolator is at
+                Integer value = (Integer) animation.getAnimatedValue();
+                // I'm going to set the layout's height 1:1 to the tick
+                mapFragment.getLayoutParams().height = value.intValue();
+                // force all layouts to see which ones are affected by
+                // this layouts height change
+                mapFragment.requestLayout();
+            }
+        });
+
+        slideAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {}
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                Intent chatIntent = new Intent(getApplicationContext(), ChatActivity.class);
+                startActivity(chatIntent);
+                overridePendingTransition(0, 0);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {}
+            @Override
+            public void onAnimationRepeat(Animator animator) {}
+        });
+
+        set = new AnimatorSet();
     }
 
     public void onClick(View v) {
@@ -40,6 +94,15 @@ public class MainActivity extends BaseActivity {
 
                 break;
 
+            case R.id.gotoChatBtn:
+                set.play(slideAnimator);
+                set.setInterpolator(new AccelerateDecelerateInterpolator());
+                set.start();
+
+                break;
+
+            case R.id.postWriteBtn:
+                break;
         }
     }
 }
