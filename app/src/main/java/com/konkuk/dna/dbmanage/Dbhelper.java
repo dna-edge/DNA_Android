5ac5b5920ee5af1656a5ccb78bc6360c386f2978
idjoopal <idjoopal@naver.com>
2018-10-20 00:44:55 +0900

    Update inner DBHelper

--- app/src/main/java/com/konkuk/dna/dbmanage/Dbhelper.java
+++ app/src/main/java/com/konkuk/dna/dbmanage/Dbhelper.java
@@ -1,14 +1,17 @@
 package com.konkuk.dna.dbmanage;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
+import java.util.HashMap;
+
 public class Dbhelper extends SQLiteOpenHelper {
 
     /*
-    * 업데이트를 하다가 혹시 디비구조가 변경되면 반드시 버전 숫자를 올려주어야 함
+    * 업데이트를 하다가 디비구조가 변경되면 *반드시* 버전 숫자를 올려주어야 함
     * */
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "DNATokenDB.db";
@@ -35,29 +38,49 @@ public class Dbhelper extends SQLiteOpenHelper {
                     DNAEntry._ID + " INTEGER PRIMARY KEY," +
                     DNAEntry.COLUME_NAME_AUTHTOKEN +  " TEXT," +
                     DNAEntry.COLUME_NAME_REFRESHTOKEN +  " TEXT," +
-                    DNAEntry.COLUME_NAME_IDX +  " TEXT," +
+                    DNAEntry.COLUME_NAME_IDX +  " INTEGER," +
                     DNAEntry.COLUME_NAME_ID +  " TEXT," +
                     DNAEntry.COLUME_NAME_EMAIL +  " TEXT," +
                     DNAEntry.COLUME_NAME_NICKNAME +  " TEXT," +
                     DNAEntry.COLUME_NAME_AVATAR +  " TEXT," +
                     DNAEntry.COLUME_NAME_DESCRIPTION +  " TEXT," +
-                    DNAEntry.COLUME_NAME_RADIUS +  " TEXT," +
-                    DNAEntry.COLUME_NAME_POINTS +  " TEXT," +
-                    DNAEntry.COLUME_NAME_ANONIMITY +  " TEXT," +
-                    DNAEntry.COLUME_NAME_FAULTY +  " TEXT," +
+                    DNAEntry.COLUME_NAME_RADIUS +  " INTEGER," +
+                    DNAEntry.COLUME_NAME_POINTS +  " INTEGER," +
+                    DNAEntry.COLUME_NAME_ANONIMITY +  " INTEGER," +
+                    DNAEntry.COLUME_NAME_FAULTY +  " INTEGER," +
                     DNAEntry.COLUME_NAME_CREATEDAT +  " TEXT )";
 
     public Dbhelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /*
+    * DB가 존재하지 않을 경우 호출
+    * */
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_ENTRIES);
     }
 
+    /*
+    * DB버전이 바뀔 경우 호출
+    * */
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    /*
+    * 유저정보 저장 메소드
+    * */
+    public void saveUserInfo(HashMap<String, String> map){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(DNAEntry.COLUME_NAME_AUTHTOKEN, map.get("authToken").toString());
+        values.put(DNAEntry.COLUME_NAME_REFRESHTOKEN, map.get("refreshToken").toString());
+        values.put(DNAEntry.COLUME_NAME_IDX, Integer.parseInt(map.get("idx").toString()));
+        values.put(DNAEntry.COLUME_NAME_ID, map.get("id").toString());
+        //뭘 저장해야 하는가?
     }
 }
