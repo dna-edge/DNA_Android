50640a82192dc72f89401d35627d0f6340a0251d
SoyoungPark <3457soso@naver.com>
2018-10-29 11:04:15 +0900

    bugfix

--- app/src/main/java/com/konkuk/dna/chat/ChatActivity.java
+++ app/src/main/java/com/konkuk/dna/chat/ChatActivity.java
@@ -17,6 +17,7 @@ import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
